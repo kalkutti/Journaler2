7a5a239ea6cec49080de0d179b4eebdce235a726
kalkutti <kalindu.sembakutti@gmail.com>
2019-12-24 16:32:54 -0500

    Toolbar

--- app/src/main/java/com/mix/journaler2/activity/MainActivity.kt
+++ app/src/main/java/com/mix/journaler2/activity/MainActivity.kt
@@ -4,12 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.mix.journaler2.R
 import com.mix.journaler2.fragment.MainFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar2)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.fragment_container, MainFragment.newInstance())
---
+++ app/src/main/java/com/mix/journaler2/fragment/TodoFragment.kt
@@ -0,0 +1,16 @@
+package com.mix.journaler2.fragment
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import com.mix.journaler2.R
+
+class TodoFragment : ItemFragment() {
+
+    override fun getLayout() = R.layout.fragment_todo
+
+}
