23cdff601e3568c8b64d4d5ec4baea484f22058d
kalkutti <kalindu.sembakutti@gmail.com>
2019-12-24 20:11:59 -0500

    Toolbar Menu

--- app/src/main/java/com/mix/journaler2/activity/MainActivity.kt
+++ app/src/main/java/com/mix/journaler2/activity/MainActivity.kt
@@ -2,11 +2,17 @@ package com.mix.journaler2.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
 import com.mix.journaler2.R
 import com.mix.journaler2.fragment.MainFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main, menu)
+        return true
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
